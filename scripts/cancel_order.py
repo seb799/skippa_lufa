4c53d4f20865539b65b0e95fdd6adbffe5af9045
Sebastien <26636348+seb799@users.noreply.github.com>
2022-04-20 23:34:22 -0400

    Add comments to order cancellation script

--- scripts/cancel_order.py
+++ scripts/cancel_order.py
@@ -23,38 +23,44 @@ def cancel_order(email:str, password:str, driver_path:str='../drivers/chromedriv
         password (str): Password used to login on lufa.com
         driver_path (str): Path or location of the chrome webdriver.exe file
     """
+    #Open Chromium browser from chromdriver
     driver = webdriver.Chrome('../drivers/chromedriver.exe')
     
+    #Reach Lufa homepage
     driver.get('https://montreal.lufa.com/en/login')
     
+    # Find email input and enter email
     user_email_element = driver.find_element_by_id('LoginForm_user_email')
-    
     user_email = driver.find_element_by_id('LoginForm_user_email')
     for letter in LUFA_EMAIL:
         time.sleep(random.random())
         user_email.send_keys(letter)
     
+    # Find password input and enter password
     user_pw = driver.find_element_by_id('LoginForm_password')
     for letter in LUFA_PW:
         time.sleep(random.random())
         user_pw.send_keys(letter)
     
+    # Submit login form
     user_pw.send_keys(Keys.RETURN)
-    
     time.sleep(2)
     
+    # Reach Lufa account page
     driver.get('https://montreal.lufa.com/en/account')
-    
     time.sleep(1)
     
+    # Find the cancel order button and click
     cancel_order = driver.find_element_by_xpath('//div[contains(@class, "order-details-buttons")]/*/span[contains(text(), "Cancel order")]')
     cancel_order.click()
     
     time.sleep(2)
     
+    # Find the cancel confirmation button and click
     confirm_cancel = driver.find_element_by_xpath('//div[contains(@class, "cancel-buttons-pop-up")]/*/div[contains(text(), "Cancel my basket")]')
     confirm_cancel.click()
     
+    # Close the browser
     driver.close()
     
     
